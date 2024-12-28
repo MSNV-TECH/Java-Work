30061ee9a65444352ebac90ab834a7e6c417d895
JKP India <188671194+JKPIN@users.noreply.github.com>
2024-12-28 11:34:27 +0000

    Pending work.

--- CheckLogic.java
+++ CheckLogic.java
@@ -2,10 +2,27 @@ import java.util.*;
 
 class CheckLogic
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         {
             System.out.println("This is the result.");
+            String response = "This is the result.";
+            positionOfFirstWord = getFirstWordPosition(response);
+            if(positionOfFirstWord == 1) 
+                System.out.println("First word is: ", response);
+            else
+            {
+                
+            }
         }
     }
+
+    static int getFirstWordPosition(String word_passed)
+    {
+        if(!word_passed.contains(" "))
+            return 1;
+        else
+            return word_passed.indexOf(" ");
+    }   
 
 }
