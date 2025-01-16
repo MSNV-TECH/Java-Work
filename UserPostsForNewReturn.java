0df696c03acabce7550f650a15476e720aeeff02
JKP India <188671194+JKPIN@users.noreply.github.com>
2025-01-16 07:30:23 +0000

    UPFNR

---
+++ UserPostsForNewReturn.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Type;
+
+class UserPostsForNewReturn
+{
+    public static void main(String[] args) 
+    {
+        Type vType;
+        Integer<T> ter_num;
+        
+    }
+
+    
+    static Req getPostRequest()
+    {
+
+    }
+
+    static Ret getReturn()
+    {
+
+    }
+
+    static String getUsers()
+    {
+        return "";
+    }
+}
